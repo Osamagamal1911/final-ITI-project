46e460943bf65657f641568fcba2ac55cdb1f26e
OsamaGamal1911 <g.osama1553@gmail.com>
2023-11-04 12:17:22 +0200

    update script

--- Terraform/Compute/startup_script.sh
+++ Terraform/Compute/startup_script.sh
@@ -3,14 +3,16 @@ sudo apt-get update
 
 sudo apt-get install kubectl
 sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
-gcloud container clusters get-credentials cluster --region us-east1 --project 
+gcloud container clusters get-credentials cluster --region us-east1 --project osama-project-401213
+git clone https://github.com/Osamagamal1911/final-ITI-project.git
+
 sudo apt install -y docker.io
 
 sudo systemctl start docker
 sudo systemctl enable docker
 
 gcloud container clusters get-credentials privatecluster --region us-east1 --project osama-project-401213
-git clone https://github.com/Osamagamal1911/final-ITI-project.git
+
 cd final-ITI-project
 cd osama
 sudo docker build -t osama-app .
