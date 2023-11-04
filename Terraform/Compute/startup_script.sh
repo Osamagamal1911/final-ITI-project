024bb1fc92efb63408814a9607b9869d1678de16
Osamagamal1911 <g.osama1553@gmail.com>
2023-11-04 13:42:17 +0200

    update script

--- Terraform/Compute/startup_script.sh
+++ Terraform/Compute/startup_script.sh
@@ -4,7 +4,6 @@ sudo apt-get update
 sudo apt-get install kubectl
 sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
 gcloud container clusters get-credentials cluster --region us-east1 --project osama-project-401213
-git clone https://github.com/Osamagamal1911/final-ITI-project.git
 
 sudo apt install -y docker.io
 
@@ -12,11 +11,12 @@ sudo systemctl start docker
 sudo systemctl enable docker
 
 gcloud container clusters get-credentials privatecluster --region us-east1 --project osama-project-401213
+# git clone https://github.com/Osamagamal1911/final-ITI-project.git
 
-cd final-ITI-project
-cd osama
-sudo docker build -t osama-app .
-sudo docker tag osama-app us-central1-docker.pkg.dev/osama-project-401213/images/osama-app
-sudo gcloud auth configure-docker us-central1-docker.pkg.dev
-sudo gcloud auth print-access-token | sudo docker login -u oauth2accesstoken --password-stdin  us-central1-docker.pkg.dev
-sudo docker push us-central1-docker.pkg.dev/osama-project-401213/images/osama-app
+# cd final-ITI-project
+# cd osama
+# sudo docker build -t osama-app .
+# sudo docker tag osama-app us-central1-docker.pkg.dev/osama-project-401213/images/osama-app
+# sudo gcloud auth configure-docker us-central1-docker.pkg.dev
+# sudo gcloud auth print-access-token | sudo docker login -u oauth2accesstoken --password-stdin  us-central1-docker.pkg.dev
+# sudo docker push us-central1-docker.pkg.dev/osama-project-401213/images/osama-app
